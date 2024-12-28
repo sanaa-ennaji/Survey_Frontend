8825c572948ac73783aa8fdf6e31cdcc02da8dca
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-28 16:20:11 +0100

    add subject service

--- src/app/services/subject.service.ts
+++ src/app/services/subject.service.ts
@@ -1,13 +1,27 @@
 import {Injectable} from '@angular/core';
 import {HttpClient } from '@angular/common/http';
-import {observable} from 'rxjs';
+import {Observable} from 'rxjs';
 
 
 export interface Subject {
     id : number ;
     title : number ;
     parentSubjectId: number; 
-    subSubjects : {id : number; title: string}[
-        
-    ]
+    subSubjects : {id : number; title: string}[];
+    surveyEdition: {id: number; creationDate:string;  startDate: string; year: number}   | null;
+    question: any[];
+
 }
+
+@Injectable({
+    providedIn : 'root',
+})
+export class SubjectService{
+    private apiUrl = 'http://localhost:8080/api/v1/subject'
+    constructor(private http: HttpClient){}
+
+    getAllSubjects():  Observable<Subject[]>{
+        return this.http.get<Subject[]>(this.apiUrl);
+    }
+}
+ 
