da7fab810505e6639180b557f155eaf03345b3f1
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-28 16:28:00 +0100

    Displays subjects and their sub-subjects in the template.

--- src/app/subjects/subjects-list/subjects-list.component.ts
+++ src/app/subjects/subjects-list/subjects-list.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SubjectService, Subject } from '../../services/subject.service';
 @Component({
   selector: 'app-subjects-list',
   imports: [],
   templateUrl: './subjects-list.component.html',
   styleUrl: './subjects-list.component.css'
 })
-export class SubjectsListComponent {
+export class SubjectsListComponent implements OnInit {
+  private subjectService = inject(SubjectService);
+  private route = inject(ActivatedRoute);
 
+  subjects: Subject[] = [];
+  filteredSubjects: Subject[] = [];
+  surveyEditionId: number | null = null;
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.surveyEditionId = Number(params['id']);
+      this.fetchSubjects();
+    });
+  }
+
+  fetchSubjects(): void {
+    this.subjectService.getAllSubjects().subscribe((data) => {
+      this.subjects = data;
+      this.filterSubjectsBySurveyEdition();
+    });
+  }
+
+  filterSubjectsBySurveyEdition(): void {
+    if (this.surveyEditionId) {
+      this.filteredSubjects = this.subjects.filter(
+        (subject) => subject.surveyEdition?.id === this.surveyEditionId
+      );
+    }
+  }
 }
