c7291e6ece14b5674b47b93918b24322a7e46930
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-21 01:21:33 +0100

    add icon

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Question } from '../models/question.model';
 import { AnswerComponent } from '../answer/answer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css'],
-  imports: [CommonModule, AnswerComponent]
+  imports: [CommonModule, AnswerComponent, FontAwesomeModule]
 })
 export class QuestionsComponent {
   @Input() questions: Question[] = [];
