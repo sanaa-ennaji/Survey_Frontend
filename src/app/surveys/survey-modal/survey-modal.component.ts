5d3cdc15a787b7fccfd0a488042a8e227ec9f600
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-22 23:16:15 +0100

    add component

---
+++ src/app/surveys/survey-modal/survey-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-modal',
+  imports: [],
+  templateUrl: './survey-modal.component.html',
+  styleUrl: './survey-modal.component.css'
+})
+export class SurveyModalComponent {
+
+}
