19bac69a2e96c8b3cbdb7d1725bfe569dbeeb9b1
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-27 20:29:02 +0100

    update routes

--- src/app/surveys/survey-modal/survey-modal.component.ts
+++ src/app/surveys/survey-modal/survey-modal.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-survey-modal',
-  imports: [],
   templateUrl: './survey-modal.component.html',
-  styleUrl: './survey-modal.component.css'
+  styleUrls: ['./survey-modal.component.css'],
 })
 export class SurveyModalComponent {
+  showModal = false;
+  surveyTitle = '';
+  surveyDescription = '';
 
+  @Output() surveyCreated = new EventEmitter<{ title: string; description: string }>();
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  createSurvey() {
+    if (this.surveyTitle && this.surveyDescription) {
+      this.surveyCreated.emit({
+        title: this.surveyTitle,
+        description: this.surveyDescription,
+      });
+      this.closeModal();
+    }
+  }
 }
