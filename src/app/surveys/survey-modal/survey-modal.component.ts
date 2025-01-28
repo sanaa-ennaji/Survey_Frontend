5a23563a3f5d2ead9b9471a8a6fc86f106220f4a
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-28 17:47:50 +0100

    Inject SurveyService into SurveyModalComponent

--- src/app/surveys/survey-modal/survey-modal.component.ts
+++ src/app/surveys/survey-modal/survey-modal.component.ts
@@ -1,18 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
+import { SurveyService } from '../../services/survey.service';
+import { SurveyCreate } from '../../models/survey-create.model';
+
 @Component({
   selector: 'app-survey-modal',
   templateUrl: './survey-modal.component.html',
   styleUrls: ['./survey-modal.component.css'],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class SurveyModalComponent {
   showModal = false;
   surveyTitle = '';
   surveyDescription = '';
+  
+  @Input() ownerId: number = 0; 
 
   @Output() surveyCreated = new EventEmitter<{ title: string; description: string }>();
+
+  constructor(private surveyService: SurveyService) {}
 
   openModal() {
     this.showModal = true;
@@ -23,12 +30,21 @@ export class SurveyModalComponent {
   }
 
   createSurvey() {
-    if (this.surveyTitle && this.surveyDescription) {
-      this.surveyCreated.emit({
+    if (this.surveyTitle && this.surveyDescription && this.ownerId) {
+      const newSurvey: SurveyCreate = {
         title: this.surveyTitle,
-        description: this.surveyDescription
+        description: this.surveyDescription,
+        ownerId: this.ownerId, // Dynamically set from the parent
+      };
+
+      this.surveyService.createSurvey(newSurvey).subscribe({
+        next: (response) => {
+          console.log('Survey created successfully:', response);
+          this.surveyCreated.emit(response); 
+          this.closeModal();
+        },
+        error: (error) => console.error('Error creating survey:', error),
       });
-      this.closeModal();
     }
-  }  
+  }
 }
