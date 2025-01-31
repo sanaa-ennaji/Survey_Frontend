9beee41517b632c8be8835d9527a0f7f32df9952
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-01 00:03:29 +0100

    update question model

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -1,9 +1,10 @@
 import { answer } from './answer.models';
 export interface Question {
-    id: number;
+    id?: number;
     text: string;
-    answerCount: number;
+    answerCount?: number;
     questionType: 'MULTIPLE_CHOICE' | 'SINGLE_CHOICE';
+    subjectId?: number;
     answers: answer[];
   }
   
