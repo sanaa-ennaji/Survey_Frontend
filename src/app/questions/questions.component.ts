d2e6505b63fd444ae633e0997ab2cb0b41de8e64
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-01 00:12:16 +0100

    Update the QuestionsComponent to include the logic for creating a new question.

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -3,21 +3,54 @@ import { CommonModule } from '@angular/common';
 import { Question } from '../models/question.model';
 import { AnswerComponent } from '../answer/answer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms'; 
+import { QuestionService } from '../services/question.service';
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css'],
-  imports: [CommonModule, AnswerComponent, FontAwesomeModule]
+  imports: [CommonModule, AnswerComponent, FontAwesomeModule, FormsModule]
 })
 export class QuestionsComponent {
   @Input() questions: Question[] = [];
   selectedAnswers: any[] | null = null;
+  newQuestion: Partial<Question> = {
+    text: '',
+    questionType: 'MULTIPLE_CHOICE',
+    subjectId: this.subjectId, 
+  };
 
+  constructor(private questionService: QuestionService) {}
   showAnswers(answers: any[]) {
     this.selectedAnswers = answers;
   }
 
   hideAnswers() {
     this.selectedAnswers = null;
+  }
+  createQuestion() {
+    if (!this.newQuestion.text || !this.newQuestion.questionType) {
+      alert('Please fill out all fields.');
+      return;
+    }
+
+    this.questionService.createQuestion(this.newQuestion as Question).subscribe({
+      next: (createdQuestion) => {
+        this.questions.push(createdQuestion); 
+        this.resetForm();
+      },
+      error: (err) => {
+        console.error('Error creating question:', err);
+        alert('Failed to create question. Please try again.');
+      },
+    });
+  }
+  
+  private resetForm() {
+    this.newQuestion = {
+      text: '',
+      questionType: 'MULTIPLE_CHOICE',
+      subjectId: this.subjectId,
+    };
   }
 }
