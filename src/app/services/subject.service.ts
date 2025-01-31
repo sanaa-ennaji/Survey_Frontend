cc940e1b260702c4cc1b270fe9c9e4dac6a18c34
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-31 21:59:51 +0100

    change the url

--- src/app/services/subject.service.ts
+++ src/app/services/subject.service.ts
@@ -9,7 +9,7 @@ import  {SubjectResponseDTO } from '../models/subject.model'
 })
 export class SubjectService {
   private baseUrl = 'http://localhost:8080/api/v1/subject/edition';
-
+  private createUrl = 'http://localhost:8080/api/v1/subject';
   constructor(private http: HttpClient) {}
 
   getSubjectsByEditionId(id: number): Observable<SubjectResponseDTO[]> {
@@ -17,7 +17,7 @@ export class SubjectService {
   }
 
   createSubject(editionId: number, subject:SubjectResponseDTO ): Observable <SubjectResponseDTO> {
-    return this.http.post<SubjectResponseDTO>(`${this.baseUrl}/${editionId}`, subject);
+    return this.http.post<SubjectResponseDTO>(`${this.createUrl}`, subject);
  
 }
 
