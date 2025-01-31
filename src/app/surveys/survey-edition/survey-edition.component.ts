b9d63d1330bc7e5d9a6603cbaef62b85cb01b9d9
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-01-31 18:13:25 +0100

    update survey edition creation

--- src/app/surveys/survey-edition/survey-edition.component.ts
+++ src/app/surveys/survey-edition/survey-edition.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SurveyEditionService } from '../../services/surveyEdition.service';
+import { SurveyEdition } from '../../models/survey-edition.model';
 @Component({
   selector: 'app-survey-edition',
   imports: [],
@@ -7,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './survey-edition.component.css'
 })
 export class SurveyEditionComponent {
+  @Input() surveyId!: number; 
+  @Output() surveyEditionCreated = new EventEmitter<SurveyEdition>();
+  
+  surveyEdition: SurveyEdition = {
+    creationDate: new Date().toISOString(),
+    startDate: '',
+    year: new Date().getFullYear(),
+  };
 
+  constructor(private surveyEditionService: SurveyEditionService) {}
+
+  createSurveyEdition(): void {
+    this.surveyEdition.surveyId = this.surveyId;
+    this.surveyEditionService.createSurveyEdition(this.surveyEdition).subscribe({
+      next: (createdEdition) => {
+        this.surveyEditionCreated.emit(createdEdition);
+        this.resetForm();
+      },
+      error: (error) => {
+        console.error('Error creating survey edition', error);
+      },
+    });
+  }
+
+  resetForm(): void {
+    this.surveyEdition = {
+      creationDate: new Date().toISOString(),
+      startDate: '',
+      year: new Date().getFullYear(),
+    };
+  }
 }
