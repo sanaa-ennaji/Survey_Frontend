25fe0b003cd92c98964f6e52c55b2ec0926ae0c0
sanaa-ennaji <sanaaennaji93@gmail.com>
2025-02-01 14:29:42 +0100

    Update the AnswerComponent to include the logic for creating a new answer.

--- src/app/answers/answer/answer.component.ts
+++ src/app/answers/answer/answer.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { AnswerService } from '../../services/answer.service'; 
 import { answer } from '../../models/answer.models';
 
 @Component({
@@ -7,8 +9,42 @@ import { answer } from '../../models/answer.models';
   standalone: true,
   templateUrl: './answer.component.html',
   styleUrls: ['./answer.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule], 
 })
 export class AnswerComponent {
   @Input() answers: answer[] = [];
+  @Input() questionId!: number; 
+  newAnswer: Partial<answer> = {
+    text: '',
+    questionId: this.questionId, 
+  };
+
+  constructor(private answerService: AnswerService) {}
+
+  createAnswer() {
+    if (!this.newAnswer.text) {
+      alert('Please fill out the answer text.');
+      return;
+    }
+
+    this.newAnswer.questionId = this.questionId;
+
+    this.answerService.createAnswer(this.newAnswer as answer).subscribe({
+      next: (createdAnswer) => {
+        this.answers.push(createdAnswer); 
+        this.resetForm(); 
+      },
+      error: (err) => {
+        console.error('Error creating answer:', err);
+        alert('Failed to create answer. Please try again.');
+      },
+    });
+  }
+
+  private resetForm() {
+    this.newAnswer = {
+      text: '',
+      questionId: this.questionId,
+    };
+  }
 }
